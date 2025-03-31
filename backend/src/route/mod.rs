161763ff3a2ab7e415aae255aa73bd6c24ff60e6
waitsalt <waitsalt@outlook.com>
2025-03-31 22:10:33 +0800

    update

---
+++ backend/src/route/mod.rs
@@ -0,0 +1,23 @@
+mod comment;
+mod user;
+mod util;
+
+use axum::Router;
+use tower_http::{
+    cors::{Any, CorsLayer},
+    trace,
+};
+
+pub fn init() -> Router {
+    let user_router = user::init();
+    let util_router = util::init();
+    let cors = CorsLayer::new()
+        .allow_origin(Any)
+        .allow_methods(Any)
+        .allow_methods(Any);
+    Router::new()
+        .nest("/api/user", user_router)
+        .nest("/api/util", util_router)
+        .layer(trace::TraceLayer::new_for_http())
+        .layer(cors)
+}
---
+++ backend/src/route/util.rs
@@ -0,0 +1,12 @@
+use axum::{Router, routing::get};
+
+use crate::service;
+
+pub fn init() -> Router {
+    Router::new()
+        .route(
+            "/captcha_email/{user_email}",
+            get(service::util::captcha::email),
+        )
+        .route("/captcha_image", get(service::util::captcha::image))
+}
